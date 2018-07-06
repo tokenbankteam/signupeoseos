581df2413b3be7e298ee2cc55fc5f033c6436dd7
thbourlove <thbourlove@gmail.com>
2018-07-06 14:33:41 +0800

    comment newaccount action

--- signupeoseos.cpp
+++ signupeoseos.cpp
@@ -4,7 +4,7 @@
 
 #include "signupeoseos.hpp"
 using namespace std;
-using namespace eosio::chain;
+//using namespace eosio::chain;
 
 void signupeoseos::transfer(account_name from, account_name to, asset quantity, string memo) {
     if (from == _self || to != _self) {
@@ -15,8 +15,8 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
     eosio_assert(quantity.is_valid(), "Invalid token transfer");
     eosio_assert(quantity.amount > 0, "Quantity must be positive");
 
-    asset stake_net(0.001, S(4, EOS));
-    asset stake_cpu(0.001, S(4, EOS));
+    asset stake_net(10, S(4, EOS));
+    asset stake_cpu(10, S(4, EOS));
     asset buy_ram = quantity - stake_net - stake_cpu;
     eosio_assert(buy_ram.amount > 0, "Not enough eos to buy ram");
 
@@ -29,24 +29,24 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
 
     account_name new_account_name = string_to_name(account_name_str.c_str());
 
-    authority owner = authority{
-        .threshold = 1,
-        .keys = {{public_key_type(public_key_str), 1}},
-        .accounts = {},
-        .waits = {}
-    };
-    newaccount new_account = newaccount{
-        .creator = _self,
-        .name = new_account_name,
-        .owner = owner,
-        .active = owner
-    };
-    action(
-            permission_level{ _self, N(active) },
-            N(eosio),
-            N(newaccount),
-            new_account
-    ).send();
+//    authority owner = authority{
+//        .threshold = 1,
+//        .keys = {{public_key(public_key_str), 1}},
+//        .accounts = {},
+//        .waits = {}
+//    };
+//    newaccount new_account = newaccount{
+//        .creator = _self,
+//        .name = new_account_name,
+//        .owner = owner,
+//        .active = owner
+//    };
+//    action(
+//            permission_level{ _self, N(active) },
+//            N(eosio),
+//            N(newaccount),
+//            new_account
+//    ).send();
 
     action(
             permission_level{ _self, N(active)},
--- signupeoseos.hpp
+++ signupeoseos.hpp
@@ -5,11 +5,22 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/types.hpp>
-#include <eosio/chain/types.hpp>
-#include <eosio/chain/authority.hpp>
-#include <eosio/chain/contract_types.hpp>
+#include <eosio.system/native.hpp>
+//#include <eosio/chain/types.hpp>
+//#include <eosio/chain/authority.hpp>
+//#include <eosio/chain/contract_types.hpp>
 
 using namespace eosio;
+
+class signup_public_key {
+public:
+    uint32_t        type;
+    std::array<char,33> data;
+
+    explicit signup_public_key(const std::string& base58str) {
+
+    }
+};
 
 class signupeoseos: public contract {
 public:
@@ -17,14 +28,18 @@ public:
     void transfer(account_name from, account_name to, asset quantity, std::string memo);
 private:
 //    struct key_weight {
-//        std::string key;
+//        public_key key;
+//        uint16_t weight;
+//    };
+//    struct wait_weight {
+//        uint32_t wait_sec;
 //        uint16_t weight;
 //    };
 //    struct authority {
 //        uint32_t threshold;
-//        std::vector<key_weight> keys;
-//        std::vector<uint64_t> accounts;
-//        std::vector<uint64_t> waits;
+//        std::vector<eosiosystem::key_weight> keys;
+//        std::vector<eosiosystem::permission_level_weight> accounts;
+//        std::vector<wait_weight> waits;
 //    };
 //    struct newaccount {
 //        account_name creator;
