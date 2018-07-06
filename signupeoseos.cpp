ca93e3a6af0eb92c467ae47ad05aabc66375cdd6
thbourlove <thbourlove@gmail.com>
2018-07-07 03:25:09 +0800

    optimize

--- signupeoseos.cpp
+++ signupeoseos.cpp
@@ -3,15 +3,12 @@
 //
 
 #include "signupeoseos.hpp"
-using namespace std;
-//using namespace eosio::chain;
 
 void signupeoseos::transfer(account_name from, account_name to, asset quantity, string memo) {
     if (from == _self || to != _self) {
         return;
     }
-    print("from: ", from, " to: ", to, " quantity: ", quantity, " memo: ", memo, "\n");
-    eosio_assert(quantity.symbol == string_to_symbol(4, "EOS"), "EosRAMBettor only accepts EOS for deposits");
+    eosio_assert(quantity.symbol == string_to_symbol(4, "EOS"), "signupeoseos only accepts EOS for signup eos account");
     eosio_assert(quantity.is_valid(), "Invalid token transfer");
     eosio_assert(quantity.amount > 0, "Quantity must be positive");
 
@@ -20,45 +17,80 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
     asset buy_ram = quantity - stake_net - stake_cpu;
     eosio_assert(buy_ram.amount > 0, "Not enough eos to buy ram");
 
-    // todo: trim string
+    memo.erase(memo.begin(), find_if(memo.begin(), memo.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+    memo.erase(find_if(memo.rbegin(), memo.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), memo.end());
+
     auto space_pos = memo.find(' ');
     eosio_assert(space_pos != string::npos, "Public key and account name must be separated with space");
 
-    auto public_key_str = memo.substr(0, space_pos);
-    auto account_name_str = memo.substr(space_pos + 1);
+    string account_name_str = memo.substr(0, space_pos);
+    eosio_assert(account_name_str.length() == 12, "Length of account name should be 12");
+
+    string public_key_str = memo.substr(space_pos + 1);
+    eosio_assert(public_key_str.length() == 53, "Length of publik key should be 53");
 
     account_name new_account_name = string_to_name(account_name_str.c_str());
 
-//    authority owner = authority{
-//        .threshold = 1,
-//        .keys = {{public_key(public_key_str), 1}},
-//        .accounts = {},
-//        .waits = {}
-//    };
-//    newaccount new_account = newaccount{
-//        .creator = _self,
-//        .name = new_account_name,
-//        .owner = owner,
-//        .active = owner
-//    };
-//    action(
-//            permission_level{ _self, N(active) },
-//            N(eosio),
-//            N(newaccount),
-//            new_account
-//    ).send();
+    string pubkey_prefix("EOS");
+    auto result = mismatch(pubkey_prefix.begin(), pubkey_prefix.end(), public_key_str.begin());
+    eosio_assert(result.first == pubkey_prefix.end(), "Public key should be prefix with EOS");
+    auto base58substr = public_key_str.substr(pubkey_prefix.length());
+
+    vector<unsigned char> vch;
+    eosio_assert(decode_base58(base58substr, vch), "Decode pubkey failed");
+    eosio_assert(vch.size() == 37, "Invalid public key");
+
+    array<unsigned char,33> pubkey_data;
+    copy_n(vch.begin(), 33, pubkey_data.begin());
+    signup_public_key pubkey = {
+        .type = 0,
+        .data = pubkey_data,
+    };
+    key_weight pubkey_weight = {
+        .key = pubkey,
+        .weight = 1,
+    };
+    authority owner = authority{
+        .threshold = 1,
+        .keys = {pubkey_weight},
+        .accounts = {},
+        .waits = {}
+    };
+    authority active = authority{
+        .threshold = 1,
+        .keys = {pubkey_weight},
+        .accounts = {},
+        .waits = {}
+    };
+    newaccount new_account = newaccount{
+        .creator = _self,
+        .name = new_account_name,
+        .owner = owner,
+        .active = active
+    };
+
+    action(
+            permission_level{ _self, N(active) },
+            N(eosio),
+            N(newaccount),
+            new_account
+    ).send();
 
     action(
             permission_level{ _self, N(active)},
             N(eosio),
             N(buyram),
-            std::make_tuple(_self, new_account_name, buy_ram)
+            make_tuple(_self, new_account_name, buy_ram)
     ).send();
 
     action(
             permission_level{ _self, N(active)},
             N(eosio),
             N(delegatebw),
-            std::make_tuple(_self, new_account_name, stake_net, stake_cpu, true)
+            make_tuple(_self, new_account_name, stake_net, stake_cpu, true)
     ).send();
 }
--- signupeoseos.hpp
+++ signupeoseos.hpp
@@ -5,48 +5,44 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/types.hpp>
-#include <eosio.system/native.hpp>
-//#include <eosio/chain/types.hpp>
-//#include <eosio/chain/authority.hpp>
-//#include <eosio/chain/contract_types.hpp>
+#include <eosiolib/action.hpp>
 
 using namespace eosio;
-
-class signup_public_key {
-public:
-    uint32_t        type;
-    std::array<char,33> data;
-
-    explicit signup_public_key(const std::string& base58str) {
-
-    }
-};
+using namespace std;
 
 class signupeoseos: public contract {
 public:
     signupeoseos(account_name self): contract(self){};
-    void transfer(account_name from, account_name to, asset quantity, std::string memo);
+    void transfer(account_name from, account_name to, asset quantity, string memo);
 private:
-//    struct key_weight {
-//        public_key key;
-//        uint16_t weight;
-//    };
-//    struct wait_weight {
-//        uint32_t wait_sec;
-//        uint16_t weight;
-//    };
-//    struct authority {
-//        uint32_t threshold;
-//        std::vector<eosiosystem::key_weight> keys;
-//        std::vector<eosiosystem::permission_level_weight> accounts;
-//        std::vector<wait_weight> waits;
-//    };
-//    struct newaccount {
-//        account_name creator;
-//        account_name name;
-//        authority owner;
-//        authority active;
-//    };
+    struct signup_public_key {
+        uint8_t        type;
+        array<unsigned char,33> data;
+    };
+    struct permission_level_weight {
+        permission_level permission;
+        weight_type weight;
+    };
+    struct key_weight {
+        signup_public_key key;
+        weight_type weight;
+    };
+    struct wait_weight {
+        uint32_t wait_sec;
+        weight_type weight;
+    };
+    struct authority {
+        uint32_t threshold;
+        vector<key_weight> keys;
+        vector<permission_level_weight> accounts;
+        vector<wait_weight> waits;
+    };
+    struct newaccount {
+        account_name creator;
+        account_name name;
+        authority owner;
+        authority active;
+    };
 };
 
 #define EOSIO_ABI_EX( TYPE, MEMBERS ) \
@@ -57,7 +53,7 @@ extern "C" { \
             /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
             eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
         } \
-        if( (code == self || action == N(onerror)) || (code == N(eosio.token) && action == N(transfer)) ) { \
+        if((code == N(eosio.token) && action == N(transfer)) ) { \
             TYPE thiscontract( self ); \
             switch( action ) { \
                 EOSIO_API( TYPE, MEMBERS ) \
@@ -68,3 +64,52 @@ extern "C" { \
 } \
 
 EOSIO_ABI_EX(signupeoseos, (transfer))
+
+
+const char* pszBase58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+bool decode_base58(const string& str, vector<unsigned char>& vch) {
+    const char * psz = str.c_str();
+    // Skip leading spaces.
+    while (*psz && isspace(*psz))
+        psz++;
+    // Skip and count leading '1's.
+    int zeroes = 0;
+    while (*psz == '1') {
+        zeroes++;
+        psz++;
+    }
+    // Allocate enough space in big-endian base256 representation.
+    vector<unsigned char> b256(strlen(psz) * 733 / 1000 + 1); // log(58) / log(256), rounded up.
+    // Process the characters.
+    while (*psz && !isspace(*psz)) {
+        // Decode base58 character
+        const char *ch = strchr(pszBase58, *psz);
+        if (ch == NULL)
+            return false;
+        // Apply "b256 = b256 * 58 + ch".
+        int carry = ch - pszBase58;
+        for (vector<unsigned char>::reverse_iterator it = b256.rbegin(); it != b256.rend(); it++) {
+            carry += 58 * (*it);
+            *it = carry % 256;
+            carry /= 256;
+        }
+        assert(carry == 0);
+        psz++;
+    }
+    // Skip trailing spaces.
+    while (isspace(*psz))
+        psz++;
+    if (*psz != 0)
+        return false;
+    // Skip leading zeroes in b256.
+    vector<unsigned char>::iterator it = b256.begin();
+    while (it != b256.end() && *it == 0)
+        it++;
+    // Copy result into output vector.
+    vch.reserve(zeroes + (b256.end() - it));
+    vch.assign(zeroes, 0x00);
+    while (it != b256.end())
+        vch.push_back(*(it++));
+    return true;
+}
