0fa6e1ffcc06b0c921b2ad9c22cf2313005249cc
thbourlove <thbourlove@gmail.com>
2018-07-07 12:16:47 +0800

    optimize

--- signupeoseos.cpp
+++ signupeoseos.cpp
@@ -12,11 +12,6 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
     eosio_assert(quantity.is_valid(), "Invalid token transfer");
     eosio_assert(quantity.amount > 0, "Quantity must be positive");
 
-    asset stake_net(10, S(4, EOS));
-    asset stake_cpu(10, S(4, EOS));
-    asset buy_ram = quantity - stake_net - stake_cpu;
-    eosio_assert(buy_ram.amount > 0, "Not enough eos to buy ram");
-
     memo.erase(memo.begin(), find_if(memo.begin(), memo.end(), [](int ch) {
         return !isspace(ch);
     }));
@@ -25,15 +20,14 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
     }).base(), memo.end());
 
     auto space_pos = memo.find(' ');
-    eosio_assert(space_pos != string::npos, "Public key and account name must be separated with space");
+    eosio_assert(space_pos != string::npos, "Account name and other command must be separated with space");
 
     string account_name_str = memo.substr(0, space_pos);
     eosio_assert(account_name_str.length() == 12, "Length of account name should be 12");
+    account_name new_account_name = string_to_name(account_name_str.c_str());
 
     string public_key_str = memo.substr(space_pos + 1);
     eosio_assert(public_key_str.length() == 53, "Length of publik key should be 53");
-
-    account_name new_account_name = string_to_name(account_name_str.c_str());
 
     string pubkey_prefix("EOS");
     auto result = mismatch(pubkey_prefix.begin(), pubkey_prefix.end(), public_key_str.begin());
@@ -46,6 +40,12 @@ void signupeoseos::transfer(account_name from, account_name to, asset quantity, 
 
     array<unsigned char,33> pubkey_data;
     copy_n(vch.begin(), 33, pubkey_data.begin());
+
+    asset stake_net(10, S(4, EOS));
+    asset stake_cpu(10, S(4, EOS));
+    asset buy_ram = quantity - stake_net - stake_cpu;
+    eosio_assert(buy_ram.amount > 0, "Not enough eos to buy ram");
+
     signup_public_key pubkey = {
         .type = 0,
         .data = pubkey_data,
